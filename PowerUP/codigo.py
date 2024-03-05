3afd5c4ee884d0bb8fc7523649a06a3ec8625a82
luizawander <luizasalesgithub@gmail.com>
2024-03-05 18:57:07 -0300

    leitura tabela

--- PowerUP/codigo.py
+++ PowerUP/codigo.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import pandas
 
 pyautogui.PAUSE= 0.5
 
@@ -11,7 +12,7 @@ pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
 
 #dar um delay
-time.sleep(1)
+time.sleep(2)
 
 pyautogui.click(x=539, y=382)
 pyautogui.write("luiza@gmail.com")
@@ -21,3 +22,8 @@ pyautogui.press("tab")
 pyautogui.write("auauau")
 
 pyautogui.click(x=665, y=538)
+time.sleep(3)
+
+
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)

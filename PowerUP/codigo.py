bc5dae96618516936d37cbd3ba548b61ea6f8f0c
luizawander <luizasalesgithub@gmail.com>
2024-03-05 18:43:43 -0300

    login

--- PowerUP/codigo.py
+++ PowerUP/codigo.py
@@ -11,5 +11,13 @@ pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
 
 #dar um delay
-time.sleep(5)
+time.sleep(1)
 
+pyautogui.click(x=539, y=382)
+pyautogui.write("luiza@gmail.com")
+
+pyautogui.press("tab")
+
+pyautogui.write("auauau")
+
+pyautogui.click(x=665, y=538)
